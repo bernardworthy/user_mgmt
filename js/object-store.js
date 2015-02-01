4028de3886cc46c1b9329f41f8b656857fb20baf
Bernard <worthybernard@gmail.com>
2015-01-31 20:59:47 -0500

    Completed Code sans styling

--- js/object-store.js
+++ js/object-store.js
@@ -5,4 +5,35 @@
 //
 function ObjectStore() {
   // TODO: implement the object store class
+  var collection = [];
+
+  var self = {
+    exists: function(obj) {
+      return collection.some(function(item){
+        return obj.equal(item);
+      });
+    },
+
+    add: function(obj) {
+      if(!self.exists(obj)) {
+        collection.push(obj);
+        return true;
+      }
+      return false;
+    },
+
+    query: function() {
+      return collection;
+    },
+
+    remove: function(obj) {
+      collection = collection.filter(function(item){
+        return !obj.equal(item);
+      });
+    }
+
+
+  };
+
+  return self;
 }
